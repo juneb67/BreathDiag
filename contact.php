41f7d77298e2506d787ed792bc0050ed364056a4
June Bowman <juneb67@gmail.com>
2015-10-22 19:06:08 -0400

    Changed contact form so it used text mail instead of html mail.

--- contact.php
+++ contact.php
@@ -1,15 +1,11 @@
 <?php
-	$title = "Contact Breath Diagnostics, Inc.";
-	require 'includes/header.php';
+
+$title = "Contact Breath Diagnostics, Inc.";
+require 'includes/header.php';
 
 function fnSanitizeStringr($str) // letters and space only for name
 {
 	return preg_replace('/[^A-Za-z\s ]/', '', $str);
-}
-
-function fnSanitizeComments($str) // only 'String' is allowed eg. '<br>HELLO</br>' => 'HELLO'
-{
-	return filter_var($str, FILTER_SANITIZE_STRIPPED); 
 }
 
 function fnValidateEmail($email) // Validates email address
@@ -48,47 +44,21 @@ if (isset($_POST['submitted'])) {
 		$name = fnSanitizeStringr($name);
 		echo "<p>Thank you, $name.</p><p>We will get back with you as soon as possible.</p><p>Have a great day!</p>";
 		$comments = $_POST["comments"];
-		$comments = fnSanitizeComments($comments);
 		$comments = stripslashes($comments);
-		//$comments = htmlspecialchars_decode($comments);
-		
+		$comments = strip_tags($comments);
 	
-$to = "juneb67@gmail.com";
-$subject = "Breath Diagnostics, Inc. Contact Form Submission";
-
-$message = "
-<html>
-<head>
-<title>Breath Diagnostics, Inc. Contact Form Submission</title>
-</head>
-<body>
-<p>This contact form was submitted through the website.</p>
-<p>Name: $name:</p>
-<p>Email: $email</p>
-<p>Comments: $comments</p>
-</body>
-</html>
-";
-
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: ' . $name . ' <' . $email . '>' . "\r\n";
-//$headers .= 'Cc: myboss@example.com' . "\r\n";
-
-mail($to,$subject,$message,$headers);
+		$to = "juneb67@gmail.com";
+		$subject = "Breath Diagnostics, Inc. Contact Form Submission";
+			
+		$msg = "This contact form was submitted through the website.\n\n";
+		$msg .= "Name: $name\n\n";
+		$msg .= "Email: $email\n\n";
+		$msg .= "Comments: $comments\n\n";
+					
+		// More headers
+		$headers .= 'From: ' . $name . ' <' . $email . '>' . "\r\n";
 		
-		
-		
-		
-		
-		
-		
-		
-		
-
+		mail($to,$subject,$msg,$headers);
 		
 	} else {
 		echo '<h4 class="bright">All fields are required. User your browser\'s back button and fill in all information.</h4>';
@@ -96,22 +66,6 @@ mail($to,$subject,$message,$headers);
 		die();
 	}
 }
-
-/* Send the email
-$to = 'juneb67@gmail.com';
-$subject = 'Breath Diagnostics, Inc. Contact Form Submission';
-$message = $comments;
-$headers = 'From: ' . $email . "\r\n" .
-    'Reply-To: ' . $email . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);
-
-*/
-
-
-
-
 
 	require 'includes/footer.php';
 
